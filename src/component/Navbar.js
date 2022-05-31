8fd7b643e6a519e42c1f279669f8500902df126f
IHECHI <nnatahi57@gmail.com>
2022-05-31 23:34:13 +0100

    travel journal

---
+++ src/component/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-icon">
+          icon
+      </div>
+      <div>
+      my travel journal.
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
