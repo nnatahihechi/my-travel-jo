43f59b30763a041db15bf6e6f2219474a7686759
IHECHI <nnatahi57@gmail.com>
2022-05-31 23:33:25 +0100

    travel journal

---
+++ src/data.js
@@ -0,0 +1,35 @@
+
+const travelData = [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        date: "12 Jan, 2021 - 24 Jan, 2021",
+        // endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.ount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+
+    {
+        title: "Sydney Opera House",
+        location: "Australia",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        date: "27 May, 2021 - 8 Jun, 2021",
+        // endDate: "24 Jan, 2021",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildingsount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://res.cloudinary.com/dgxbiyast/image/upload/v1654035944/Austrilia_y4vubf.jpg"
+    },
+    {
+        title: "Geirangerfjord ",
+        location: "Norway",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        date: "01 Oct, 2021 - 18 Nov, 2021",
+        // endDate: "24 Jan, 2021",
+        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://res.cloudinary.com/dgxbiyast/image/upload/v1654035964/Norway_y3h7jc.jpg"
+    }
+
+
+]
+
+export default travelData
